6b4a5fe1abed33cc74fe695f6fdde55302f54ed8
Mads Krohn <sniffdk@gmail.com>
2017-01-24 15:06:53 +0100

    more builzzz

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -1,29 +1,24 @@
 echo Running build-appveyor.cmd now ...
 
+echo nuget
 nuget restore
 
-echo Full build of solution takes place
-msbuild PocDeploy.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+# echo Full build of solution takes place
+# msbuild PocDeploy.sln /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
-echo Hello ? :(
+echo frontend
 
 call npm --version
 call node --version
-
-echo Plz
-
 call npm install bower -g
 call npm install gulp-cli -g
-
-cd PocDeploy
-
-echo Be nice :)
-
 call npm install
 call bower install
 
-echo JavaScript files created from TypeScript are turned into build artifacts by Gulp
+cd $(APPVEYOR_PROJECT_NAME).Web
+
+echo gulp
 call gulp build --production
 
-echo Everything that has been built gets packaged
-msbuild PocDeploy.csproj /t:Package /p:PackageLocation=PocDeploy.zip
+echo build
+msbuild /t:Package /p:PackageLocation=$(APPVEYOR_PROJECT_NAME).Web.zip
