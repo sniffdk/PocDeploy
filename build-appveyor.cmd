46c87acac5d4236b1cf2d187fbf0b9c4051f2d42
Mads Krohn <sniffdk@gmail.com>
2017-01-24 13:12:39 +0100

    !!

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -9,11 +9,19 @@ ECHO Hello ? :(
 
 node --version
 npm --version
+
+ECHO Plz
+
 npm install bower -g
 npm install gulp-cli -g
+
 cd PocDeploy
+
+ECHO Be nice :)
+
 npm install
 bower install
+
 gulp build --production
 
 # JavaScript files created from TypeScript are turned into build artifacts by Gulp
